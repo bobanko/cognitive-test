b2ed7657e0aa56fac72e5d4812d5633e2ceb83da
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-04-15 02:54:05 +0300

    feat: firebase leaderboards impl WIP

--- firebase.js
+++ firebase.js
@@ -1,8 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-analytics.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getFirestore,
+  query,
+  orderBy,
+  limit,
+  collection,
+  getDocs,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+import {
+  getAuth,
+  signInAnonymously,
+  //   GoogleAuthProvider,
+  //   EmailAuthProvider,
+  //   linkWithCredential,
+} from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,4 +31,73 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+export async function loadLeaderboards() {
+  //read list
+
+  const leaderboardsRef = collection(db, "leaderboards");
+
+  const querySnapshot = getDocs(
+    query(leaderboardsRef, orderBy("score"), limit(50))
+  );
+
+  return querySnapshot;
+}
+
+export async function saveHiScores({ uid, score, date }) {
+  //write
+  try {
+    const docRef = await addDoc(collection(db, "leaderboards"), {
+      uid,
+      score,
+      date,
+    });
+
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+export async function getUser() {
+  const auth = getAuth();
+
+  try {
+    const { user } = await signInAnonymously(auth);
+
+    // Signed in..
+    console.log("signed anon", user);
+
+    return user;
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ...
+
+    console.log(errorMessage, errorCode);
+    return null;
+  }
+}
+
+// todo(vmyshko): convert anon to registered
+// https://firebase.google.com/docs/auth/web/anonymous-auth#web-modular-api_3
+
+// const email = "test@test.com";
+// const password = "qwerty";
+
+// const credential = GoogleAuthProvider.credential(
+//     googleUser.getAuthResponse().id_token);
+
+// const credential = EmailAuthProvider.credential(email, password);
+
+// console.log(credential);
+
+// linkWithCredential(auth.currentUser, credential)
+//   .then((usercred) => {
+//     const user = usercred.user;
+//     console.log("Anonymous account successfully upgraded", user);
+//   })
+//   .catch((error) => {
+//     console.log("Error upgrading anonymous account", error);
+//   });
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const cellCount = 25;
+import { Timer } from "./timer.js";
+import { avatars } from "./avatars.js";
+import { getUser, loadLeaderboards, saveHiScores } from "./firebase.js";
+import { getHashNum } from "./helpers.js";
+
+// const cellCount = 25;
+const cellCount = 4;
 const $cellGrid = document.querySelector(".cell-grid");
 const $timer = document.querySelector(".timer");
 const $resetBtn = document.querySelector(".reset");
@@ -9,7 +15,6 @@ const $root = document.querySelector(":root");
 $root.style.setProperty("--cellCount", cellCount);
 
 $cellGrid.dataset.cellCount = cellCount;
-var interval = null;
 
 function initGrid() {
   //remove all
@@ -17,7 +22,53 @@ function initGrid() {
 
   let currentNum = 1;
 
-  function onClick($cell) {
+  function addLeaderboardsRow({ uid, rank, score, date, isCurrent }) {
+    const rowFragment = $tmplLeaderboardsRow.content.cloneNode(true);
+
+    //rank
+    const $rank = rowFragment.querySelector(".lb-rank");
+    $rank.textContent = rank;
+    //player
+    const avatarId = getHashNum(uid, avatars.length);
+    const $player = rowFragment.querySelector(".lb-player");
+    $player.textContent = avatars[avatarId];
+    //score
+    const $score = rowFragment.querySelector(".lb-score");
+    $score.textContent = formatScore(score);
+    //date
+    const $date = rowFragment.querySelector(".lb-date");
+    $date.textContent = date.toDate().toLocaleString();
+
+    if (isCurrent) {
+      const $tr = rowFragment.querySelector("tr");
+      $tr.classList.add("current");
+    }
+
+    $leaderboardsTableBody.appendChild(rowFragment);
+  }
+
+  const timer = new Timer();
+
+  timer.onUpdate((diff) => {
+    const timeStr = new Date(diff).toLocaleTimeString("en-US", {
+      minute: "numeric",
+      second: "numeric",
+      hour12: false,
+    });
+
+    $timer.textContent = timeStr;
+  });
+
+  function getAvatarForUid(uid) {
+    const avatarId = getHashNum(uid, avatars.length);
+    return avatars[avatarId];
+  }
+
+  function formatScore(score) {
+    return `${score / 1000}s`;
+  }
+
+  async function onClick($cell) {
     if ($cell.classList.contains("checked")) return;
 
     if (+$cell.dataset.number !== currentNum) return;
@@ -27,7 +78,42 @@ function initGrid() {
 
     if (currentNum > cellCount) {
       //win
-      clearInterval(interval);
+
+      timer.stop();
+
+      // todo(vmyshko): show win and leaders
+
+      const user = await getUser();
+
+      $avatar.textContent = getAvatarForUid(user.uid);
+
+      const currentScore = timer.getDiff();
+
+      $score.textContent = formatScore(currentScore);
+
+      saveHiScores({
+        uid: user.uid,
+        score: currentScore,
+        date: new Date(),
+      });
+
+      $overlay.hidden = false;
+      const leaders = await loadLeaderboards();
+
+      //clear fake data
+      $leaderboardsTableBody.replaceChildren();
+
+      let rank = 0;
+      leaders.forEach((doc) => {
+        const userData = doc.data();
+
+        console.log(doc.id, userData);
+
+        const isCurrent =
+          userData.uid === user.uid && userData.score === currentScore;
+        rank++;
+        addLeaderboardsRow({ ...userData, rank, isCurrent });
+      });
     }
   }
 
@@ -46,24 +132,7 @@ function initGrid() {
 
   $cellGrid.append(...preCells);
 
-  const timeStart = new Date().getTime();
-
-  clearInterval(interval);
-  timerInterval();
-  function timerInterval() {
-    const timeNow = new Date().getTime();
-    const diff = timeNow - timeStart;
-
-    var timeStr = new Date(diff).toLocaleTimeString("en-US", {
-      minute: "numeric",
-      second: "numeric",
-      hour12: false,
-    });
-
-    $timer.textContent = timeStr;
-  }
-
-  interval = setInterval(timerInterval, 1000);
+  timer.start();
 }
 
 //
