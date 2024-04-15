5afd3e2e2ca1971b9340ce52d7cc7864856c76d6
Volodymyr Myshko <v.myshko.synergetica@gmail.com>
2024-04-16 02:44:32 +0300

    refac: configs extract WIP

---
+++ config.js
@@ -0,0 +1,20 @@
+export const cellCount = 25;
+// export const cellCount = 4;
+
+// todo(vmyshko): switch between modes?
+// export const hiScoresTableName = "hiScores-2x2";
+export const hiScoresTableName = "hiScores-5x5";
+// export const leaderboardsTableName = "leaderboards"; // todo(vmyshko): can't rename from fb, need to write code for that migration
+// export const leaderboardsTableName = "leaderboards-5x5"; // todo(vmyshko): can't rename from fb, need to write code for that migration
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCvdAyCXjT8-rxTgx-_Hza6qivdwzrjW3s",
+  authDomain: "cognitive-test-83d3f.firebaseapp.com",
+  projectId: "cognitive-test-83d3f",
+  storageBucket: "cognitive-test-83d3f.appspot.com",
+  messagingSenderId: "84067554932",
+  appId: "1:84067554932:web:5404e84daa589863866321",
+  measurementId: "G-LM7L0F5E3N",
+};
